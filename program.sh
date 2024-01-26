660ff26b13caf1b28395b77afec8eea440c68c7d
olivier <olivier@factoryz.fr>
2024-01-26 14:20:19 +0100

    Create program for the Calculator

--- program.sh
+++ program.sh
@@ -1,3 +1,59 @@
 #! /bin/bash
 
 echo "Bienvenue dans la Calculatrice en Bash"
+
+continue=1
+while test $continue -eq 1
+do
+
+echo " "
+
+echo "Selectionnez une opération : "
+echo "1. Addition"
+echo "2. Soustraction"
+echo "3. Multiplication"
+echo "4. Division"
+
+echo " "
+
+echo "Entrez le numéro de l'opération :"
+read operation
+
+echo " "
+echo "Entrez le premier nombre :"
+read nbOne
+
+echo " "
+echo "Entrez le deuxième nombre :"
+read nbTwo
+
+echo " "
+
+if test $operation -eq 1
+then echo resultat : $((nbOne + nbTwo))
+
+elif test $operation -eq 2
+then echo resultat : $((nbOne - nbTwo))
+
+elif test $operation -eq 3
+then echo resultat : $((nbOne * nbTwo))
+
+elif test $operation -eq 4
+then echo resultat : $((nbOne / nbTwo))
+
+else echo "Veuillez indiquer une opération valide"
+fi
+
+echo " "
+echo "Voulez-vous effectuer une autre opération ? (Oui/Non) :"
+read response
+
+if test $response == "Oui"
+then continue=1
+
+elif test $response == "Non"
+then continue=0
+
+fi
+
+done
